be1cdc1a745afab4375c7b3ac11bf88f67091832
Starov Nikita <starovnikita15@gmail.com>
2021-01-05 15:11:23 +0200

    Initial commit

---
+++ src/main/java/com/sprint11/Sprint11/S11Team35Application.java
@@ -0,0 +1,13 @@
+package com.sprint11.Sprint11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S11Team35Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S11Team35Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprint11/Sprint11/S11Team35ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint11.Sprint11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S11Team35ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
